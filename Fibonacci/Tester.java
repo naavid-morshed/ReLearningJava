9842721e00b137686fd52c04e32d21119bba78b2
Naavid Morshed <naavid.m@gmail.com>
2023-10-29 17:59:13 +0600

    updated fibonacci

--- Fibonacci/Tester.java
+++ Fibonacci/Tester.java
@@ -1,5 +1,4 @@
 package Fibonacci;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,11 +13,21 @@ public class Tester {
 
         new Fibonacci(fibonacciList, length);
 
-        System.out.print("Output: ");
+        System.out.print("Forward  : ");
+        for (int counter = 0; counter < fibonacciList.size(); counter++) {
+            String fibonacciElement = fibonacciList.get(counter);
+            if ((counter < fibonacciList.size() - 1)) {
+                System.out.print(fibonacciElement + ", ");
+            } else {
+                System.out.print(fibonacciElement + ".");
+                System.out.println();
+            }
+        }
 
-        for (int i = 0; i < fibonacciList.size(); i++) {
-            String fibonacciElement = fibonacciList.get(i);
-            if (!(i == fibonacciList.size() - 1)) {
+        System.out.print("Backwards: ");
+        for (int counter = fibonacciList.size() - 1; counter >= 0; counter--) {
+            String fibonacciElement = fibonacciList.get(counter);
+            if ((counter > 0)) {
                 System.out.print(fibonacciElement + ", ");
             } else {
                 System.out.print(fibonacciElement + ".");
