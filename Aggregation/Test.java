42b7f7413b4679a3324b1da9059b533120d6f471
Naavid Morshed <naavid.m@gmail.com>
2023-10-31 12:04:28 +0600

    asdf

--- Aggregation/Test.java
+++ Aggregation/Test.java
@@ -6,10 +6,9 @@ public class Test {
 //         I should use it here as it Emp has a "has" relation with address
 //         if there's an "is" relation then inheritance would be a better choice
         Address address2 = new Address("Detroit", "California", "USA");
+        Emp employee2 = new Emp(111, "John Denver", address2);
 
-        Emp employee1 = new Emp(111, "Naavid Morshed",
-                new Address("Chittagong", "CTG", "Bangladesh"));
-        Emp employee2 = new Emp(112, "John Denver", address2);
+        Emp employee1 = new Emp(112, "Naavid Morshed", new Address("Chittagong", "CTG", "Bangladesh"));
 
         employee1.display();
         employee2.display();
--- RunTimePoly/Splendor.java
+++ RunTimePoly/Splendor.java
@@ -20,7 +20,7 @@ final class Splendor extends Bike {
 
 class Test{
     public static void main(String[] args) {
-        Bike b = new Splendor();//upcasting
+        Bike b = new Splendor(); //upcasting
         b.run();
 
         System.out.println();
