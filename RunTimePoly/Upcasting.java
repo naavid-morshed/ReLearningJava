06f158565569e1124a396c42cd4aed666b49758c
Naavid Morshed <naavid.m@gmail.com>
2023-11-07 18:53:51 +0600

    asdf

--- RunTimePoly/Upcasting.java
+++ RunTimePoly/Upcasting.java
@@ -5,6 +5,7 @@ public class Upcasting {
         Child obj1 = new Child();
         Parent obj2 = new Child(); // obj2 can only access overridden methods from child or methods from parent
         System.out.println(obj1.methodOne((float) 2));
+        System.out.println(obj2.methodOne(1,2)); // overridden method runs
     }
 }
 
@@ -21,6 +22,11 @@ class Child extends Parent {
     public int methodOne(int a) {
         System.err.println("Inside Child error() method");
         return 10 + a;
+    }
+
+    public int methodOne(int a, int b) {
+        System.err.println("Inside Parent error() method");
+        return a + b +b;
     }
 
     public float methodOne(float a) {
